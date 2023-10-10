8acb256b7c5bfeada559f822ac917c8f787febe9
Harlequine <cs.joshuakylepocampo@gmail.com>
2023-10-10 16:23:07 +0800

    Added Controllers

---
+++ src/main/java/com/harlequine/MoviesAPI/Models/Movie.java
@@ -0,0 +1,35 @@
+package com.harlequine.MoviesAPI.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movie") //this class sheet presents each document in the movie collections
+//similar to js creating a model for mongodb
+@Data
+@AllArgsConstructor// an annotation for creating a constructor that takes all the private fields in this class as arguments
+@NoArgsConstructor// similar to AllargsConstructor but takes no parameters/arguments
+public class Movie {
+
+    @Id //annotation for the framework to know that this is the unique identifier for each data inside the collection in the database.
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String startDate;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+    @DocumentReference // Manual Reference relationship between Movie collection and Review Collection
+    private List<Review> reviewId;// unlike in js where you create a model where its field has a value of an object, and you simply write it as is inline. In Java, you need a blueprint or in OOP we call it a class to construct the structure of the value of that field.
+
+
+}
--- src/main/java/com/harlequine/MoviesAPI/MoviesApiApplication.java
+++ src/main/java/com/harlequine/MoviesAPI/MoviesApiApplication.java
@@ -1,14 +1,24 @@
 package com.harlequine.MoviesAPI;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;// this class has a method that runs the spring application
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+//annotations in java are usually used to let the compiler know what this class does
 @SpringBootApplication
+//@RestController // Lets the framework know that this is a Rest API controller and not just an ordinary class
 public class MoviesApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesApiApplication.class, args)
-		;
+		SpringApplication.run(MoviesApiApplication.class, args);
 	}
+
+	 // Lets the framework that this method is an API endpoint
+//	@ResponseBody
+//	@GetMapping("/root")
+//	public String apiRoot(){
+//		return "hello world";
+//	}
 
 }
